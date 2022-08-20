a2ac49adb7db8b72bce0aa3735480c4e8651f76c
Muhammadsuleman31 <98824481+Muhammadsuleman31@users.noreply.github.com>
2022-08-20 17:47:03 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import path from "path"
+
+const app = express();
+
+app.use(express.static("build"))
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'build',"index.html"))
+})
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server listening on 5000");
+  });
